29756cec1416b49c86532258ecc737edfb38ca4f
jaimaurya38 <maurya.jaiprakash38@gmail.com>
2025-01-23 12:04:04 +0530

    Selecting State with Selectors

--- src/app/products/products-page/products-page.component.ts
+++ src/app/products/products-page/products-page.component.ts
@@ -1,22 +1,22 @@
-import { Component } from '@angular/core';
-import { sumProducts } from 'src/app/utils/sum-products';
-import { Product } from '../product.model';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ProductsService } from '../products.service';
 import { Store } from '@ngrx/store';
 import { ProductsAPIActions, ProductsPageActions } from '../state/products.actions';
-import { state } from '@angular/animations';
+import { selectProducts, selectProductsLoading, selectProductsShowProductCode, selectProductsCount, selectProductsTotal } from '../state/products.selectors';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
   styleUrls: ['./products-page.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductsPageComponent {
-  products$ = this.store.select((state: any) => state.product.products);;
-  total = 0;
-  loading$ = this.store.select((state: any) => !(state.product.loading));
+  products$ = this.store.select(selectProducts);
+  total$ = this.store.select(selectProductsTotal);
+  totalProductCount$ = this.store.select(selectProductsCount);
+  loading$ = this.store.select(selectProductsLoading);
   //fetch value from store using (selector state-slice)
-  showProductCod$ = this.store.select((state: any) => state.product.showProdctCode);
+  showProductCode$ = this.store.select(selectProductsShowProductCode);
   errorMessage = '';
 
   constructor(private productsService: ProductsService, private store: Store) {
@@ -33,7 +33,7 @@ export class ProductsPageComponent {
       next: (products) => {
         //this.products = products;
         this.store.dispatch(ProductsAPIActions.productsLoadSuccess({ products }));
-        this.total = sumProducts(products);
+        //this.total = sumProducts(products);
       },
       error: (error) => (this.errorMessage = error),
     });
@@ -42,6 +42,6 @@ export class ProductsPageComponent {
   toggleShowProductCode() {
     // update state by calling dispatch
     //this.store.dispatch({ type: '[Product Page] Toggle show Product code' });
-    this.store.dispatch(ProductsPageActions.toggleShowProductCode())
+    this.store.dispatch(ProductsPageActions.toggleShowProductCode());
   }
 }
---
+++ src/app/products/state/products.selectors.ts
@@ -0,0 +1,32 @@
+import { createFeature, createFeatureSelector, createSelector } from "@ngrx/store";
+import { ProductsState } from "./products.reducer";
+import { sumProducts } from "src/app/utils/sum-products";
+
+const countOfProducts = (state: ProductsState) => state.products.length;
+
+export const selectProductsState = createFeatureSelector<ProductsState>('product');
+
+export const selectProducts = createSelector(
+    selectProductsState,
+    (state) => state.products
+);
+
+export const selectProductsShowProductCode = createSelector(
+    selectProductsState,
+    (state) => state.showProdctCode
+);
+
+export const selectProductsLoading = createSelector(
+    selectProductsState,
+    (state) => state.loading
+);
+
+export const selectProductsCount = createSelector(
+    selectProductsState,
+    countOfProducts
+);
+
+export const selectProductsTotal = createSelector(
+    selectProductsState,
+    (state) => sumProducts(state.products)
+);
